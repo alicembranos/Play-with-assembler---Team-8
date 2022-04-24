0d8e960cafb96b88129515e71e432f9e9c7fd295
jose-cuevas <jgutifer@gmail.com>
2022-04-24 16:30:40 +0200

    new changes

---
+++ src/functions.js
@@ -0,0 +1,7 @@
+function showVisibility(idElement, lastElement){
+    idElement.classList.toggle("hide");
+    lastElement.classList.toggle("hide");
+    console.log("presionado");
+}
+
+export default {showVisibility};
