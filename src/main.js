f414ba0f58a40b743f074af39ece0055ba2b24e5
JcarlosCabello1991 <jc.cabellomangas6@gmail.com>
2022-04-25 16:08:35 +0200

    logical finish

--- src/main.js
+++ src/main.js
@@ -17,105 +17,167 @@ const boardGame = document.getElementById("boardGame");
 const clickElement = document.getElementById("clickElement");
 
 const listScore = document.getElementById("listScore");
+const downcounter = document.getElementById("downcounter");
+
+let counterClicks = 0;
+let interval;
+let intervalGame;
 
 // Storing user info in objects
 
 let user = {
-  username: "",
-  //   currentPlaying: true,
-  scores: 0,
+    username: "",
+    //   currentPlaying: true,
+    scores: 0,
 };
 
-// let arrayRanking = [
-//   {
-//     username: "Cris",
-//     scores: 6,
-//   },
-//   {
-//     username: "Carlos",
-//     scores: 9,
-//   },
-//   {
-//     username: "Alicia",
-//     scores: 1,
-//   },
-// ];
+let arrayRanking = [
+    {
+        username: "Cris",
+        scores: 6,
+    },
+    {
+        username: "Carlos",
+        scores: 9,
+    },
+    {
+        username: "Alicia",
+        scores: 1,
+    },
+];
 
-let arrayRanking;
+//let arrayRanking = [];
 
-// localStorage.setItem("ranking", JSON.stringify(arrayRanking));
+localStorage.setItem("ranking", JSON.stringify(arrayRanking));
 
 upLoadRanking();
 
 startBtn.addEventListener("click", function () {
-  if (validateForm()) {
-    loadUser(userNameInput.value);
+    if (validateForm()) {
+        loadUser(userNameInput.value);
 
-    showVisibility(wireFrame1, wireFrame2);
-  }
+        showVisibility(wireFrame1, wireFrame2);
+    }
 });
 
 start.addEventListener("click", function () {
-  showVisibility(wireFrame2, wireFrame3);
+    showVisibility(wireFrame2, wireFrame3);
+    timerReady();
 });
 
 playAgain.addEventListener("click", function () {
-  showVisibility(wireFrame5, wireFrame3);
+    showVisibility(wireFrame5, wireFrame3);
+    timerReady();
 });
 
 /****** Functions for Wireframe 4 (Game) *******/
 clickElement.addEventListener("click", function () {
-  movingElement();
+    counterClicks++;
+    movingElement();
 });
 
 // FUNCTIONS
 function loadUser(userName, scores = "current playing") {
-  //
-  user.username = userName;
-  user.scores = scores;
+    //
+    user.username = userName;
+    user.scores = scores;
 
-  arrayRanking.unshift(user);
+    if (arrayRanking === "null") {
+        arrayRanking.push(user);
+    } else {
+        arrayRanking.unshift(user);
+    }
+    createList(arrayRanking);
 }
 
-loadUser();
-
 function upLoadRanking() {
-  arrayRanking = JSON.parse(localStorage.getItem("ranking"));
-  listScore.textContent = "";
-
-  if (arrayRanking !== null) {
-    arrayRanking.sort(function (obj1, obj2) {
-      return obj2.scores - obj1.scores;
-    });
-
-    for (const ranking of arrayRanking) {
-      const liElement = document.createElement("li");
-      const parr1 = document.createElement("p");
-      const parr2 = document.createElement("p");
-      parr1.textContent = ranking.username;
-      parr2.textContent = ranking.scores;
-      parr1.setAttribute("class", "nameBold");
-      parr2.setAttribute("class", "score");
-      liElement.appendChild(parr1);
-      liElement.appendChild(parr2);
-      listScore.appendChild(liElement);
+    console.log(localStorage.getItem("ranking"));
+    if (localStorage.getItem("ranking") !== "null") {
+        arrayRanking = JSON.parse(localStorage.getItem("ranking"));
+        createList(arrayRanking);
     }
-  }
 }
 
 function finalStorageScore(score) {
-  arrayRanking[0].scores = score;
-  localStorage.setItem("ranking", JSON.stringify(arrayRanking));
+    arrayRanking[0].scores = score;
+    localStorage.setItem("ranking", JSON.stringify(arrayRanking));
 }
 
 function validateForm() {
-  const regex = /^[^\s]+$/;
-  if (userNameInput.value === "" || userNameInput.value === Number) {
-    errorMessage.textContent = "Please insert a name";
-    return false;
-  } else if (!regex.test(userNameInput.value)) {
-    errorMessage.textContent = "Please insert a name whitout blank spaces";
-    return false;
-  }
-  return true;
+    const regex = /^[^\s]+$/;
+    if (userNameInput.value === "" || userNameInput.value === Number) {
+        errorMessage.textContent = "Please insert a name";
+        return false;
+    } else if (!regex.test(userNameInput.value)) {
+        errorMessage.textContent = "Please insert a name whitout blank spaces";
+        return false;
+    }
+    return true;
+}
+
+//Timer of step three
+function timerReady() {
+    let timeToStart = 4;
+    interval = setInterval(function () {
+        //show countdown
+        downcounter.textContent = timeToStart-1;
+        console.log(downcounter.textContent);
+        timeToStart--;
+        if (timeToStart == 0) {
+            stopReady();
+        }
+    }, 1000);
+}
+
+function stopReady() {
+    setTimeout(function () {
+        clearInterval(interval, 1000);
+    }, 1000);
+    showVisibility(wireFrame3, wireFrame4);
+    timerGame();
+}
+
+//Timer of GAME
+function timerGame(){
+    let timeToEnd = 10;
+    intervalGame = setInterval(function () {
+        //show countdown
+        console.log(timeToEnd);
+        timeToEnd--;
+        if (timeToEnd == 0) {
+            stopGame();
+        }
+    }, 1000);
+}
+
+function stopGame(){
+    setTimeout(function () {
+        clearInterval(intervalGame, 1000);
+    }, 5)
+    showVisibility(wireFrame4, wireFrame5);
+    arrayRanking[0].scores = counterClicks;
+    counterClicks = 0;
+    createList(arrayRanking);
+    localStorage.setItem('ranking', JSON.stringify(arrayRanking));
+}
+
+function createList(array) {
+    array.sort(function (obj1, obj2) {
+        return obj2.scores - obj1.scores;
+    });
+
+    listScore.textContent = "";
+
+    for (const item of array) {
+        const liElement = document.createElement("li");
+        const parr1 = document.createElement("p");
+        const parr2 = document.createElement("p");
+        parr1.textContent = item.username;
+        parr2.textContent = item.scores;
+        parr1.setAttribute("class", "nameBold");
+        parr2.setAttribute("class", "score");
+        liElement.appendChild(parr1);
+        liElement.appendChild(parr2);
+        listScore.appendChild(liElement);
+    }
 }
